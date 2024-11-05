f376e1e02456b78a6f3fdef375236bf974a3752a
Rahul4444741 <77558879+Rahul4444741@users.noreply.github.com>
2024-11-05 08:20:58 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'sm': '548px',
+      // => @media (min-width: 448px) { ... }
+
+      'md': '748px',
+      // => @media (min-width: 1024px) { ... }
+
+      'lg': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
+  plugins: [],
+}
+
